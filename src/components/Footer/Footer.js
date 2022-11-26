a84fc8b0d9805e7bb85aa6b84b7ca512014592e6
Abbey <abbeysalvo@gmail.com>
2022-11-26 15:52:06 -0500

    Ensured footer is responsive and accessible, and updated more flexible responsive style classes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,13 +11,14 @@ import { siteNavigation } from "../..";
 const Footer = ({ ...props }) => {
   return (
     <footer className="footer" role="contentinfo">
-      <div className="footer__content">
+      <div className="footer__content flex--justify-space-between flex--column tablet:flex--row width-full">
         <div className="footer__column">
           <a className="footer__logo" href="/">
             <svg
               className="logo__image"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 27.88 32.37"
+              width="100%"
             >
               <polygon
                 vector-effect="non-scaling-stroke"
@@ -27,72 +28,11 @@ const Footer = ({ ...props }) => {
             <h1 className="logo__name">Hide-A-Way</h1>
           </a>
         </div>
-        <div className="footer__column">
-          <div className="footer__contact">
-            <h2 className="footer__content_title">Contact</h2>
-            <ul className="footer__nav-list">
-              <li>
-                <a
-                  className="footer__nav-link"
-                  href="tel:8889284534"
-                  aria-label="8 8 8. 9 2 8. 4 5 3 4."
-                >
-                  (888) 928-4534
-                </a>
-              </li>
-              <li>
-                <a
-                  className="footer__nav-link"
-                  href="mailto:sales@hideawaydoors.com"
-                >
-                  sales@hideawaydoors.com
-                </a>
-              </li>
-              <li>
-                325 South Sugar Hollow Rd
-                <br /> Morristown, TN 37813
-              </li>
-              <ul className="footer__nav-list footer__nav-list--row">
-                <li>
-                  <div
-                    className="icon--social--facebook icon--outline icon--3xs"
-                    title="Facebook"
-                  >
-                    <span className="sr-only">Facebook</span>
-                  </div>
-                </li>
-                <li>
-                  <div
-                    className="icon--social--twitter icon--outline icon--3xs"
-                    title="Twitter"
-                  >
-                    <span className="sr-only">Twitter</span>
-                  </div>
-                </li>
-                <li>
-                  <div
-                    className="icon--social--youtube icon--outline icon--3xs"
-                    title="YouTube"
-                  >
-                    <span className="sr-only">YouTube</span>
-                  </div>
-                </li>
-                <li>
-                  <div
-                    className="icon--social--instagram icon--outline icon--3xs"
-                    title="Instagram"
-                  >
-                    <span className="sr-only">Instagram</span>
-                  </div>
-                </li>
-              </ul>
-            </ul>
-          </div>
-        </div>
-        <div className="footer__column">
-          <h2 className="footer__content_title">Quick Links</h2>
-          <div className="footer__quick-links">
-            <ul className="footer__nav-list">
+
+        <div className="footer__content flex--wrap margin-left-auto">
+          <div className="footer__column">
+            <h2 className="footer__content_title">Quick Links</h2>
+            <ul className="footer__nav-list flex--gap-2xs">
               {siteNavigation.map(({ path, title }) => (
                 <li>
                   {/**
@@ -118,46 +58,111 @@ const Footer = ({ ...props }) => {
               </li>
             </ul>
           </div>
-        </div>
-        <div className="footer__column">
-          <h2 className="footer__content_title">Payment Methods</h2>
-          <ul className="footer__nav-list footer__nav-list--row">
-            <li>
-              <div
-                className="icon--payment--amex icon--fill icon--lg"
-                title="American Express"
-              >
-                <span className="sr-only">American Express</span>
-              </div>
-            </li>
-            <li>
-              <div
-                className="icon--payment--mastercard icon--fill icon--lg"
-                title="Master Card"
-              >
-                <span className="sr-only">Master Card</span>
-              </div>
-            </li>
-            <li>
-              <div
-                className="icon--payment--paypal icon--fill icon--lg"
-                title="PayPal"
-              >
-                <span className="sr-only">PayPal</span>
-              </div>
-            </li>
-            <li>
-              <div
-                className="icon--payment--visa icon--fill icon--lg"
-                title="Visa"
-              >
-                <span className="sr-only">Visa</span>
-              </div>
-            </li>
-          </ul>
+          <div className="footer__column">
+            <h2 className="footer__content_title">Contact</h2>
+            <ul className="footer__nav-list flex--gap-2xs">
+              <li>
+                <a
+                  className="footer__nav-link"
+                  href="tel:8889284534"
+                  aria-label="8 8 8. 9 2 8. 4 5 3 4."
+                >
+                  (888) 928-4534
+                </a>
+              </li>
+              <li>
+                <a
+                  className="footer__nav-link"
+                  href="mailto:sales@hideawaydoors.com"
+                >
+                  sales@hideawaydoors.com
+                </a>
+              </li>
+              <li>
+                325 South Sugar Hollow Rd
+                <br /> Morristown, TN 37813
+              </li>
+              <li>
+                <ul className="footer__nav-list footer__nav-list--row  flex--gap-2xs">
+                  <li>
+                    <a
+                      className="icon--social--facebook icon--outline icon--3xs"
+                      href="https://www.facebook.com/hideawaydoors"
+                      title="Facebook"
+                    >
+                      <span className="sr-only">Facebook</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="icon--social--twitter icon--outline icon--3xs"
+                      href="https://www.twitter.com/WayDoors"
+                      title="Twitter"
+                    >
+                      <span className="sr-only">Twitter</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="icon--social--youtube icon--outline icon--3xs"
+                      href="https://www.youtube.com/channel/UCX13r8IwUFvYtFggS8Jj9YA"
+                      title="YouTube"
+                    >
+                      <span className="sr-only">YouTube</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="icon--social--instagram icon--outline icon--3xs"
+                      href="https://www.instagram.com/hideawaydoors/"
+                      title="Instagram"
+                    >
+                      <span className="sr-only">Instagram</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div className="footer__column">
+            <ul className="footer__nav-list footer__nav-list--row">
+              <li>
+                <div
+                  className="icon--payment--amex icon--fill icon--lg"
+                  title="American Express"
+                >
+                  <span className="sr-only">American Express</span>
+                </div>
+              </li>
+              <li>
+                <div
+                  className="icon--payment--mastercard icon--fill icon--lg"
+                  title="Master Card"
+                >
+                  <span className="sr-only">Master Card</span>
+                </div>
+              </li>
+              <li>
+                <div
+                  className="icon--payment--paypal icon--fill icon--lg"
+                  title="PayPal"
+                >
+                  <span className="sr-only">PayPal</span>
+                </div>
+              </li>
+              <li>
+                <div
+                  className="icon--payment--visa icon--fill icon--lg"
+                  title="Visa"
+                >
+                  <span className="sr-only">Visa</span>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div className="footer__copyright">
+      <div className="footer__copyright flex--justify-space-between width-full">
         © 2022 by Hide-A-Way Doors, LLC
         <div className="flex margin-left-auto">
           <ul className="footer__nav-list">
